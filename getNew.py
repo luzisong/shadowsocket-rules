0d9d0c47830c86ce161ac20d95b42783f4db64ba
zisung <luzisong@outlook.com>
2023-02-24 17:37:21 +0800

    [Fri Feb 24 17:37:21 CST 2023]auto update

--- getNew.py
+++ getNew.py
@@ -1,24 +1,21 @@
 
 import requests
+import subprocess
+
+
+save_path = "./new"
 
 url = 'https://raw.githubusercontent.com/deezertidal/shadowrocket-rules/main/shadowrocket_basic.conf'
 
-proxyaddr = "127.0.0.1"    
-proxyport = 1081                     
-
-proxyurl="http://"+proxyaddr+":"+"%d"%proxyport
-
-r = requests.get(url,headers={
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "Cache-Control":"max-age=0",
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"})
+# 使用wget命令下载文件
+subprocess.call(["wget", url, "-P", save_path])               
 
 f = open('basic.conf','r')
 str1 = f.read()
 f.close()
-str2 = r.text
+f = open('new),'r')
+str2 = r.read()
+f.close()
 
 i=str2.index('[Script]')
 strNew = str1 + '' + str2[i:-1]
